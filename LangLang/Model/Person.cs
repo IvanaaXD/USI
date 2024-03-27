acd7c2f4c27b6ef09fd6f202990a64484ff7533f
Milan <milan.lazarevic03@gmail.com>
2024-03-27 12:58:45 -0700

    feature/add user base class

---
+++ LangLang/Model/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangLang.Model
+{
+    public abstract class Person
+    {
+        protected string firstName;
+        protected string lastName;
+        protected Gender gender;
+        protected DateTime dateOfBirth;
+        protected string phoneNumber;
+        protected string email;
+        protected string password;
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public Gender Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get { return dob; }
+            set { dob = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public abstract string[] ToCsv();
+        public abstract void FromCsv(string[] values);
+    }
+}
