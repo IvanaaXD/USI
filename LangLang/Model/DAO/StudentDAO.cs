edf924b717748d73eefbf4cab5ffbe8106fe6019
Marina Ivanovic <ivanovic.marina.003@gmail.com>
2024-04-29 22:51:51 +0200

    add single course view and enrolled students

--- LangLang/Model/DAO/StudentDAO.cs
+++ LangLang/Model/DAO/StudentDAO.cs
@@ -82,7 +82,8 @@ namespace LangLang.Model.DAO
             if (student.ActiveCourseId != -1)
                 teacherDAO.DecrementCourseCurrentlyEnrolled(student.ActiveCourseId);
 
-            foreach(int examTermId in student.RegisteredExamsIds) {
+            foreach (int examTermId in student.RegisteredExamsIds)
+            {
                 teacherDAO.DecrementExamTermCurrentlyAttending(examTermId);
             }
             // NotifyObservers();
@@ -114,12 +115,13 @@ namespace LangLang.Model.DAO
 
             List<Course> availableCourses = new List<Course>();
 
-            foreach(Course course in allCourses) {
-                if(!passedCoursesIds.Contains(course.Id) && 
+            foreach (Course course in allCourses)
+            {
+                if (!passedCoursesIds.Contains(course.Id) &&
                    !courseIdsByRegisteredExams.Contains(course.Id) &&
-                   !student.RegisteredCoursesIds.Contains(course.Id) && 
+                   !student.RegisteredCoursesIds.Contains(course.Id) &&
                    !student.CompletedCoursesIds.Contains(course.Id) &&
-                   (course.CurrentlyEnrolled < course.MaxEnrolledStudents) && 
+                   (course.CurrentlyEnrolled < course.MaxEnrolledStudents) &&
                    (course.StartDate - currentTime).TotalDays > 6)
                 {
                     availableCourses.Add(course);
@@ -143,7 +145,7 @@ namespace LangLang.Model.DAO
         private List<int> GetPassedCourses(Student student)
         {
             List<int> courses = new List<int>();
-            foreach(int examTermId in student.PassedExamsIds)
+            foreach (int examTermId in student.PassedExamsIds)
             {
                 ExamTerm examTerm = teacherDAO.GetExamTermById(examTermId);
                 courses.Add(examTerm.CourseID);
@@ -162,7 +164,7 @@ namespace LangLang.Model.DAO
             foreach (int courseId in student.CompletedCoursesIds)
             {
                 List<ExamTerm> examTerms = GetExamTermsByCourse(courseId);
-                foreach(ExamTerm examTerm in examTerms)
+                foreach (ExamTerm examTerm in examTerms)
                 {
                     if ((examTerm.CurrentlyAttending < examTerm.MaxStudents) &&
                         (examTerm.ExamTime - currentTime).TotalDays > 30)
@@ -178,14 +180,15 @@ namespace LangLang.Model.DAO
         private List<ExamTerm> GetExamTermsByCourse(int courseId)
         {
             Course course = teacherDAO.GetCourseById(courseId);
-            return teacherDAO.FindExamTermsByCriteria(course.Language, course.Level,null);
+            return teacherDAO.FindExamTermsByCriteria(course.Language, course.Level, null);
         }
 
         public List<Course> GetRegisteredCourses(int studentId)
         {
             Student student = GetStudentById(studentId);
             List<Course> registeredCourses = new List<Course>();
-            foreach(int courseId in student.RegisteredCoursesIds) {
+            foreach (int courseId in student.RegisteredCoursesIds)
+            {
                 registeredCourses.Add(teacherDAO.GetCourseById(courseId));
             }
 
@@ -214,6 +217,18 @@ namespace LangLang.Model.DAO
 
             return registeredCourses;
         }
+        public List<Student> GetAllStudentsForCourse(int courseId)
+        {
+            List<Student> filteredStudents = new List<Student>();
+            foreach (Student student in _students)
+            {
+                if (student.RegisteredCoursesIds.Contains(courseId))
+                {
+                    filteredStudents.Add(student);
+                }
+            }
+            return filteredStudents;
+        }
 
         public List<Student> GetAllStudentsForExamTerm(int examTermId)
         {
@@ -222,7 +237,7 @@ namespace LangLang.Model.DAO
             {
                 if (student.RegisteredExamsIds.Contains(examTermId))
                 {
-                    filteredStudents.Add(student);  
+                    filteredStudents.Add(student);
                 }
             }
             return filteredStudents;
---
+++ LangLang/View/Teacher/CourseView.xaml.cs
@@ -0,0 +1,111 @@
+﻿using LangLang.Model;
+using LangLang.Controller;
+using LangLang.DTO;
+using LangLang.Observer;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace LangLang.View.Teacher
+{
+    public partial class CourseView : Window, IObserver
+    {
+        public ObservableCollection<MailDTO> Mails { get; set; }
+        public ObservableCollection<StudentDTO> Students { get; set; }
+        public class ViewModel
+        {
+            public ObservableCollection<MailDTO> Mails { get; set; }
+            public ObservableCollection<StudentDTO> Students { get; set; }
+
+            public ViewModel()
+            {
+                Mails = new ObservableCollection<MailDTO>();
+                Students = new ObservableCollection<StudentDTO>();
+            }
+        }
+        
+        public ViewModel MailsTableViewModel { get; set; }
+        public ViewModel StudentsTableViewModel { get; set; }
+        public MailDTO SelectedMail { get; set; }
+
+        private readonly Course course;
+        private readonly Model.Teacher teacher;
+        private readonly TeacherController teacherController;
+        private readonly StudentsController studentController;
+
+        public CourseView(Course course, Model.Teacher teacher, TeacherController teacherController, StudentsController studentController)
+        {
+            InitializeComponent();
+            this.course = course;
+            this.teacherController = teacherController;
+            this.studentController = studentController;
+            this.teacher = teacher;
+
+            MailsTableViewModel = new ViewModel();
+            StudentsTableViewModel = new ViewModel();
+
+            DataContext = this;
+
+            teacherController.Subscribe(this);
+
+            if (course.StartDate.Date <= DateTime.Now.AddDays(-7))
+            {
+                PenaltyPoint.Visibility = Visibility.Collapsed;
+            }
+
+            Update();
+        }
+
+        public void Update()
+        {
+            try
+            {
+                MailsTableViewModel.Mails.Clear();
+                var mails = teacherController.GetAllMails();
+
+                if (mails != null)
+                {
+                    foreach (Mail mail in mails)
+                        MailsTableViewModel.Mails.Add(new MailDTO(mail));
+                }
+                else
+                {
+                    MessageBox.Show("No teachers found.");
+                }
+
+                StudentsTableViewModel.Students.Clear();
+                var students = studentController.GetAllStudentsForCourse(course.Id);
+
+                if (students != null)
+                {
+                    foreach (Model.Student student in students)
+                        StudentsTableViewModel.Students.Add(new StudentDTO(student));
+                }
+                else
+                {
+                    MessageBox.Show("No courses found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void PenaltyPoint_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReadMail_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AnswerMail_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
