8bb45c5d4d450d046def2e7d72bb83c4b45d1710
Milica Stanojlovic <mstanojlovic222@gmail.com>
2024-05-14 20:13:54 +0200

    add penalty point class

---
+++ LangLang/Controller/PenaltyPointController.cs
@@ -0,0 +1,47 @@
+﻿using LangLang.Model.DAO;
+using LangLang.Model;
+using LangLang.Observer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LangLang.Controller
+{
+    public class PenaltyPointController
+    {
+        private readonly PenaltyPointDAO _points;
+
+        public PenaltyPointController()
+        {
+            _points = new PenaltyPointDAO();
+        }
+        public void Add(PenaltyPoint penaltyPoint)
+        {
+            _points.AddPenaltyPoint(penaltyPoint);
+        }
+
+        public void Update(PenaltyPoint penaltyPoint)
+        {
+            _points.UpdatePenaltyPoint(penaltyPoint);
+        }
+        public void Delete(int penaltyPointId)
+        {
+            _points.RemovePenaltyPoint(penaltyPointId);
+        }
+        public List<PenaltyPoint> GetAllPenaltyPoints()
+        {
+            return _points.GetAllPenaltyPoints();
+        }
+        public List<PenaltyPoint> GetPointsByCourseId(int courseId)
+        {
+            return _points.GetPenaltyPointsByCourseId(courseId);
+        }
+        public List<PenaltyPoint> GetPenaltyPointsByStudentId(int studentId)
+        {
+            return _points.GetPenaltyPointsByStudentId(studentId);
+        } 
+    }
+}
---
+++ LangLang/Model/PenaltyPoint.cs
@@ -0,0 +1,81 @@
+﻿using LangLang.Model.Enums;
+using LangLang.Storage.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangLang.Model
+{
+    public class PenaltyPoint : ISerializable
+    {
+        private int id;
+        private int studentId;
+        private int courseId;
+        private DateTime dateSent;
+        private bool isDeleted;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public int StudentId
+        {
+            get { return studentId; }
+            set { studentId = value; }
+        }
+
+        public int CourseId
+        {
+            get { return courseId; }
+            set { courseId = value; }
+        }
+
+        public DateTime DateSent
+        {
+            get { return dateSent; }
+            set { dateSent = value; }
+        }
+
+        public bool IsDeleted
+        {
+            get { return isDeleted; }
+            set { isDeleted = value; }
+        }
+        public PenaltyPoint() { }
+        
+        protected PenaltyPoint(int id, int studentId, int courseId, DateTime dateSent, bool isDeleted)
+        {
+            this.id = id;
+            this.studentId = studentId;
+            this.courseId = courseId;
+            this.dateSent = dateSent;
+            this.isDeleted = isDeleted;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues =
+            {
+                id.ToString(),
+                studentId.ToString(),
+                courseId.ToString(),
+                dateSent.ToString("yyyy-MM-dd HH:mm"),
+                isDeleted.ToString(),
+            };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = int.Parse(values[0]);
+            studentId = int.Parse(values[1]);
+            courseId = int.Parse(values[2]);
+            dateSent = DateTime.ParseExact(values[3], "yyyy-MM-dd HH:mm", null);
+            isDeleted = bool.Parse(values[4]);
+        }
+
+    }
+}
