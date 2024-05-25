e5d7d5f5784553b574ec1e9f2fada6d1270ded81
Ivana_R <ivanar1357@gmail.com>
2024-05-25 19:05:48 +0200

    add gui for choosing teacher

--- LangLang/View/Director/ChooseTeacherView.xaml.cs
+++ LangLang/View/Director/ChooseTeacherView.xaml.cs
@@ -1,7 +1,22 @@
-﻿
+﻿using LangLang.Observer;
+using System.Windows;
+
 namespace LangLang.View.Director
 {
-    internal class ChooseTeacher
+    public partial class ChooseTeacherView : Window, IObserver
     {
+        public ChooseTeacherView() { }
+
+        public void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Choose_Click(object sender, RoutedEventArgs e)
+        { 
+        }
+
+        private void TeachersListBox_SelectionChanged(object sender, RoutedEventArgs e) { }
+
     }
 }
