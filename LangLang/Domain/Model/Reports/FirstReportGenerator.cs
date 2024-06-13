a11bd9d7f7e7cfe440304d35ed22c9c936008aca
Ivana_R <ivanar1357@gmail.com>
2024-06-13 22:15:45 +0200

    refactor report function

--- LangLang/Domain/Model/Reports/FirstReportGenerator.cs
+++ LangLang/Domain/Model/Reports/FirstReportGenerator.cs
@@ -1,9 +1,4 @@
 ﻿using LangLang.Controller;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LangLang.Domain.Model.Reports
 {
