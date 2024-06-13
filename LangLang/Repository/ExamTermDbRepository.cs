e002bf4f5cc057cba14d18dc9fc5196e6b170736
Milica Stanojlovic <mstanojlovic222@gmail.com>
2024-06-13 20:28:47 +0200

    change in contollers and repositories

--- LangLang/Repository/ExamTermDbRepository.cs
+++ LangLang/Repository/ExamTermDbRepository.cs
@@ -4,47 +4,58 @@ using LangLang.Domain.Model;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using LangLang.Observer;
+using System;
 
 namespace LangLang.Repository
 {
     public class ExamTermDbRepository : IExamTermDbRepository
     {
         private readonly AppDbContext _context;
-
+        private readonly Subject _subject;
         public ExamTermDbRepository(AppDbContext context)
         {
             _context = context;
+            _subject = new Subject();
         }
-
-        public async Task<IEnumerable<ExamTerm>> GetAllAsync()
+        public void Add(ExamTerm examTerm)
         {
-            return await _context.ExamTerms.ToListAsync();
+            _context.ExamTerms.Add(examTerm);
+            _context.SaveChanges();
+            _subject.NotifyObservers();
         }
 
-        public async Task<ExamTerm> GetByIdAsync(int id)
+        public ExamTerm GetById(int id)
         {
-            return await _context.ExamTerms.FindAsync(id);
-        }
+            var examTerm = _context.ExamTerms.Find(id);
+            if (examTerm == null)
+            {
+                throw new KeyNotFoundException($"ExamTerm with ID {id} not found.");
+            }
 
-        public async Task AddAsync(ExamTerm examTerm)
+            return examTerm;
+        }
+        public void Remove(ExamTerm examTerm)
         {
-            await _context.ExamTerms.AddAsync(examTerm);
-            await _context.SaveChangesAsync();
+            _context.ExamTerms.Remove(examTerm);
+            _context.SaveChanges();
+            _subject.NotifyObservers();
         }
-
-        public async Task UpdateAsync(ExamTerm examTerm)
+        public void Update(ExamTerm examTerm)
         {
             _context.ExamTerms.Update(examTerm);
-            await _context.SaveChangesAsync();
+            _context.SaveChanges();
+            _subject.NotifyObservers();
         }
 
-        public async Task DeleteAsync(int id)
+        public void Delete(int id)
         {
-            var examTerm = await _context.ExamTerms.FindAsync(id);
+            var examTerm = GetById(id);
             if (examTerm != null)
             {
                 _context.ExamTerms.Remove(examTerm);
-                await _context.SaveChangesAsync();
+                _context.SaveChanges();
+                _subject.NotifyObservers();
             }
         }
     }
